68bf5057369c56176f5e5bf65d8a04c137d59913
Jonathan RX <jonathan.ravix@gmail.com>
2017-06-16 08:14:45 +0200

    Utilisation de Mysqli
    Utilisation de Mysqli pour assurer la compatibilité avec PHP7

--- class.bdpoo.php
+++ class.bdpoo.php
@@ -1,5 +1,25 @@
 <?php
 
+/*
+Gestion de base de donnée
+Date : 12/2009
+Dernière modification : 06/2017 -> Utilisation de Mysqli pour compatibilité avec PHP7
+Auteur: Jonathan RAVIX
+Cette classe à pour but de simplifier l'accès à une base de donnée en utilisant une méthode de programmation proche de celle de wordpress.
+Comment l'utiliser :
+Avant de l'utiliser, il convient de définir les variables $host, $login, $password et basename en fonction de votre serveur et base de donnée.
+
+Script d'exmple :
+$mybd = new Bd;
+$mybd->config('hote','utilisateur','mot de passe','base de donnée');
+$mybd->update('matable', array('info'=>'Mon titre d\'information'), array('info'=>'My info'));
+$tables = $mybd->get_results('SELECT * FROM matable');
+print_r($tables);
+
+echo '<br /><br />' . $tables[0]->info;
+
+*/
+
 class Bd{
 	private $host = 'localhost';
 	private $login = 'root';
@@ -7,28 +27,27 @@ class Bd{
 	private $basename = 'test_poo';
 	public $prefix = '';
 	public $option_table = '';
+	private $connect = '';
 	public function insert($table_name, $data){
 		$insert_id = '';
 		$insert_value = '';
-		mysql_connect($this->host, $this->login, $this->password);
-		mysql_select_db($this->basename);
+		$connect = mysqli_connect($this->host, $this->login, $this->password,$this->basename);
 		foreach($data as $id => $value){
 		$id = addslashes($id);
 		$value = addslashes($value);
 			if($insert_id == ''){$insert_id .= '' . $id . '';}else{$insert_id .= ', ' . $id . '';}
 			if($insert_value == ''){$insert_value .= '"' . $value . '"';}else{$insert_value .= ' ,"' . $value . '"';}
 		}
-		$mysql_query = 'INSERT INTO ' . $table_name. '(' . $insert_id . ') VALUES(' . $insert_value . ')';
-		if(mysql_query($mysql_query)){
+		$mysqli_query = 'INSERT INTO ' . $table_name. '(' . $insert_id . ') VALUES(' . $insert_value . ')';
+		if(mysqli_query($connect, $mysqli_query)){
 			return true;
 		}else{
 			return false;
 		}
-		mysql_close();
+		mysqli_close();
 	}
 	public function update($table_name, $data, $where){
-		mysql_connect($this->host, $this->login, $this->password);
-		mysql_select_db($this->basename);
+		$connect = mysqli_connect($this->host, $this->login, $this->password, $this->basename);
 		$insert_value = '';
 		foreach($data as $id => $value){
 		$id = addslashes($id);
@@ -41,22 +60,21 @@ class Bd{
 			$value = addslashes($value);
 			if($where_insert == ''){$where_insert .= $id . '="' . $value . '"';}else{$where_insert .= ' AND ' . $id . '="' . $value . '"';}
 		}
-		$mysql_query = 'UPDATE ' . $table_name. ' SET ' . $insert_value . ' WHERE ' . $where_insert . '';
+		$mysqli_query = 'UPDATE ' . $table_name. ' SET ' . $insert_value . ' WHERE ' . $where_insert . '';
 		
-		if(mysql_query($mysql_query)){
+		if(mysqli_query($connect, $mysqli_query)){
 		return true;
 		}else{
 		return false;
 		}
-		mysql_close();
+		mysqli_close();
 	}
 	public function get_results($request){
-		mysql_connect($this->host, $this->login, $this->password);
-		mysql_select_db($this->basename);
-		$results = mysql_query($request);
+		$connect = mysqli_connect($this->host, $this->login, $this->password,$this->basename);
+		$results = mysqli_query($connect, $request);
 		$reponse = array();
 		$counter = 0;
-		while ($result = mysql_fetch_array($results)){
+		while ($result = mysqli_fetch_array($results)){
 			$reponse[$counter] = new stdClass;
 			foreach($result as $key => $value){
 					$reponse[$counter]->$key = stripcslashes($value);
@@ -64,14 +82,13 @@ class Bd{
 			$counter++;
 		}
 		return $reponse;
-		mysql_close();
+		mysqli_close();
 	}
 	public function query($query = ''){
-		mysql_connect($this->host, $this->login, $this->password);
-		mysql_select_db($this->basename);
-		$results = mysql_query($query);
+		$connect = mysqli_connect($this->host, $this->login, $this->password, $this->basename);
+		$results = mysqli_query($connect, $query);
 		return true;
-		mysql_close();
+		mysqli_close();
 	}
 	public function get_option($name=''){
 		$r = $this->get_results('SELECT * FROM ' . $this->option_table . ' WHERE option_name="' . $name . '"');
@@ -83,7 +100,7 @@ class Bd{
 			$reponse = false;
 		}
 		return $reponse;
-		mysql_close();
+		mysqli_close();
 	}
 	public function set_option($name='',$value=''){
 		$r = $this->get_results('SELECT * FROM ' . $this->option_table . ' WHERE option_name="' . $name . '"');
@@ -102,12 +119,8 @@ class Bd{
 		}
 	}
 	public function test_connect($host = '', $login = '', $password = '', $basename = ''){
-		if(mysql_connect($host, $login, $password)){
-			if(mysql_select_db($basename)){
+		if(mysqli_connect($host, $login, $password, $basename)){
 				return true;
-			}else{
-				return false;
-			}
 		}else{
 			return false;
 		}
@@ -121,4 +134,6 @@ class Bd{
 		$this->option_table = $option_table;
 	}
 }
+
+
 ?>
